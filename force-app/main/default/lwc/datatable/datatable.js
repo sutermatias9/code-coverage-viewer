9f60f3dff49b9194fc8ab93ed2608ae4338e43d1
Matias Suter <sutermatias9@gmail.com>
2024-10-18 17:55:51 -0300

    Update datatable LWC
    - Change cursor to pointer on row hover
    - Fire event with first cell value on row click

--- force-app/main/default/lwc/datatable/datatable.js
+++ force-app/main/default/lwc/datatable/datatable.js
@@ -3,4 +3,19 @@ import { LightningElement, api } from 'lwc';
 export default class Datatable extends LightningElement {
     @api headers;
     @api rowsData; // [[],[]...]
+
+    handleRowClick(event) {
+        event.preventDefault();
+        const firstCellValue = this.getFirstCell(event.currentTarget);
+        this.fireEvent(firstCellValue);
+    }
+
+    getFirstCell(rowElement) {
+        return rowElement.querySelector('th a').textContent;
+    }
+
+    fireEvent(detail) {
+        const event = new CustomEvent('rowclick', { detail, bubbles: true, composed: true });
+        this.dispatchEvent(event);
+    }
 }
