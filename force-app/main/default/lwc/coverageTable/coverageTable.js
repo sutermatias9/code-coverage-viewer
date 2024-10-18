ce8d1d4831e4e187013b4609b2b53083f5c48d09
Matias Suter <sutermatias9@gmail.com>
2024-10-18 16:56:05 -0300

    Rename coverageData LWC to coverageTable

--- force-app/main/default/lwc/coverageTable/coverageTable.js
+++ force-app/main/default/lwc/coverageTable/coverageTable.js
@@ -1,8 +1,10 @@
 import { LightningElement } from 'lwc';
 import runAllTests from '@salesforce/apex/ApexTestRunner.runAllTests';
 import getClassCoverages from '@salesforce/apex/ApexClassHandler.getClassCoverages';
+import getClassBody from '@salesforce/apex/ApexClassHandler.getClassBody';
 
 export default class CoverageData extends LightningElement {
+    body;
     coverageData;
 
     tableHeaders = ['Apex Class', 'Total Lines', 'Covered Lines', 'Uncovered Lines', 'Coverage'];
@@ -22,6 +24,9 @@ export default class CoverageData extends LightningElement {
             this.coverageData = await getClassCoverages();
             this.createTableData();
             console.log(JSON.stringify(this.coverageData));
+
+            this.body = JSON.parse(await getClassBody()); // puedo analizar el JSON y lo rompo en divs [[]..]
+            this.bodysplit = this.body.split('\n');
         } catch (e) {
             console.error(e);
         }
